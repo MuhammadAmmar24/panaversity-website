22b542766e07728e33fafe94c836288a7fa493ed
Ahmed <heydude08080@gmail.com>
2024-09-01 17:48:21 +0500

    add team section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
+import Courses from "@/components/Courses";
+import Hero from "@/components/Hero";
+import Team from "@/components/Team";
 
 
 export default function Home() {
   return (
     <>
-    
-    <div>Panaversity Frontend</div>
-    <div className="bg-primary text-textPrimary">Hello World</div>
-    <div className="bg-secondary text-textPrimary">Hello World</div>
-    <div className="bg-accent text-textPrimary">Hello World</div>
-    <div className="bg-primary text-textSecondary">Hello World</div>
-    <div className="bg-secondary text-textSecondary">Hello World</div>
-
+      <Hero />
+      <Courses />
+      <Team />
     </>
   );
 }
