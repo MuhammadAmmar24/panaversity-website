ee3dd83e37e4af0dc358efcc9a9d2b3b0b2636f1
Ahmed <heydude08080@gmail.com>
2024-09-01 20:45:04 +0500

    add faq-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Courses from "@/components/Courses";
 import Hero from "@/components/Hero";
 import Team from "@/components/Team";
-
+import Faq from "@/components/Faq";
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
       <Hero />
       <Courses />
       <Team />
+      <Faq />
     </>
   );
 }
