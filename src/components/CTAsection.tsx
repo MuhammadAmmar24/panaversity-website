dd02e3d2e58ec8a93ee810e96b61ea70baccd778
muhammadammar24 <jazba685@gmail.com>
2024-09-05 18:02:51 +0500

    cta content changed

--- src/components/CTAsection.tsx
+++ src/components/CTAsection.tsx
@@ -4,17 +4,17 @@
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
 import { Button } from "@/components/ui/button"
+import { ctaData } from "@/constants/cta"
 
 export default function Component() {
   return (
-    <div className="flex justify-center items-center h-[35em] sm:h-[25em]  md:h-[35rem] px-6 sm:px-[4rem] md:px[6rem] -mt-20 bg-background">
-      <div className="bg-custom-bg1 flex flex-col justify-center items-center gap-1 bg-center bg-cover rounded-3xl p-5  md:p-[3rem] lg:p-[4rem] max-w-3xl text-center">
-        <h2 className="text-[1.4rem] md:text-[1.6rem] font-bold text-white font-poppins mb-4">Read To Start Creating Stunning Images With Power </h2>
+    <div className="flex justify-center items-center h-[35em] sm:h-[25em]  md:h-[40rem] px-6 sm:px-[4rem] md:px[5rem] -mt-20 bg-background">
+      <div className="bg-custom-bg1 flex flex-col justify-center items-center gap-1 bg-center bg-cover rounded-3xl p-5  md      :p-[3rem] lg:p-[5rem] max-w-3xl md:max-w-4xl text-center">
+        <h2 className="text-[1.4rem] md:text-[2.2rem] font-bold text-white font-poppins mb-4">{ctaData.heading}</h2>
         <p className="text-[0.9rem] md:text-[1rem] mb-6 text-background font-inter">
-          Sign up today and unlock the full potential of our tool. With a range processing techniques and features at
-          your fingertips, you'll be amazed what you will achieve.
+          {ctaData.subHeading}
         </p>
-        <Button variant="outline" className="bg-background text-[#186449] hover:bg-[#218360] hover:text-background px-6 py-2 rounded-3xl">
+        <Button variant="outline" className="bg-background text-[#186449] hover:bg-[#218360] hover:text-background px-6 py-2 lg:px-8 md:py-6 rounded-3xl">
           Get Started <ArrowRightIcon className="ml-2" />
         </Button>
       </div>
