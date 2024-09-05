67a995d911f34f1c8adbdeb347333f7f342d23d2
dev-shayan <devvshayan@gmail.com>
2024-09-05 14:02:23 +0500

    enhanced faqs section

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -5,7 +5,7 @@ import { faqData } from "@/constants/faqs";
 
 export default function Faqs() {
   return (
-    <section className="h-screen m-12 mx-auto w-[20rem] sm:w-[35rem] lg:w-[42rem] xl:w-[65rem]">
+    <section className="max-w-3xl mx-auto px-4 py-8">
       <div className="flex flex-col items-center justify-center text-center mb-6 md:mb-12">
           <h2 className="text-md text-textPrimary text-center sm:text-lg gradient-border font-medium border-b rounded-[100px] mb-5  uppercase tracking-wide">
             FAQ
@@ -13,15 +13,19 @@ export default function Faqs() {
           <h2 className="text-3xl text-textPrimary font-poppins font-semibold tracking-tighter text-center sm:text-4xl md:text-5xl ">
           Top Answered Questions          </h2>
         </div>
-      <Accordion variant="light">
+      <Accordion variant="light" className="mt-10 ">
         {faqData.map((item, index) => (
           <AccordionItem
-            key={index}
-            aria-label={item.question}
-            title={item.question}
-          >
+          className="border-b border-gray-200"
+          key={index}
+          aria-label={item.question}
+          title={<span className="font-semibold text-[#031811]">{item.question}</span>}
+        >
+          <div className="text-[#031811B2] text-sm">
             {item.answer}
-          </AccordionItem>
+          </div>
+        </AccordionItem>
+        
         ))}
       </Accordion>
     </section>
