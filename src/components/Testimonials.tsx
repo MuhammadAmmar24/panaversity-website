a7bd569f5feb3cf41c75490d65aaf765b1bc82e3
dev-shayan <devvshayan@gmail.com>
2024-09-05 13:10:32 +0500

    ADDED HEADINGS IN testimonial section

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,39 +1,43 @@
 "use client";
 import React from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay'; // Import autoplay styles
-import { Autoplay, Pagination } from 'swiper/modules';
-import { testimonialList } from "@/constants/testimonials"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay"; // Import autoplay styles
+import { Autoplay, Pagination } from "swiper/modules";
+import { testimonialList } from "@/constants/testimonials";
 
-import 'swiper/css/navigation';
-
+import "swiper/css/navigation";
 
 const Testimonials = () => {
   return (
-    <section className="ezy__testimonial24 light py-14 md:py-24 mb-8 md:mb-0 bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white">
+    <section className="ezy__testimonial24 light py-14 md:py-24 mb-8 md:mb-0 bg-white dark:bg-[#0b1727] text-gray-900 ">
       <div className="container px-4 mx-auto">
-        <div className="flex items-center justify-center text-center mb-6 md:mb-12">
-          <div className="max-w-xl">
-            <hr className="w-20 mb-4 border-gray-300 dark:border-gray-600 mx-auto" />
-            <h2 className="text-[2.5rem] font-bold">What They Say</h2>
-          </div>
+        <div className="flex flex-col items-center justify-center text-center mb-6 md:mb-12">
+          <h2 className="text-md text-textPrimary text-center sm:text-lg gradient-border font-medium border-b rounded-[100px] mb-5  uppercase tracking-wide">
+            Testimonials
+          </h2>
+          <h2 className="text-3xl text-textPrimary font-poppins font-semibold tracking-tighter text-center sm:text-4xl md:text-5xl ">
+          Our User’s Speeches About Panaversity
+          </h2>
         </div>
 
         <Swiper
           slidesPerView={3}
           spaceBetween={30}
           autoplay={{ delay: 3500, disableOnInteraction: false }}
-        //   pagination={{ clickable: true }}
+          //   pagination={{ clickable: true }}
           breakpoints={{
-            240: { // For medium screens and up
-                slidesPerView: 1,
-              },
-            640: { // For medium screens and up
+            240: {
+              // For medium screens and up
+              slidesPerView: 1,
+            },
+            640: {
+              // For medium screens and up
               slidesPerView: 2,
             },
-            1024: { // For large screens and up
+            1024: {
+              // For large screens and up
               slidesPerView: 3,
             },
           }}
