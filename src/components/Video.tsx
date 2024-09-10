28878aeadfb72562bcdd682a4ec3366ae8b6b0a0
muhammadammar24 <jazba685@gmail.com>
2024-09-10 16:17:50 +0500

    program section fixes

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -6,8 +6,7 @@ export default function Video() {
     <>
       <div>
         <div className="flex bg-background items-center justify-center h-[45rem] px-3  mt-[-12rem] sm:mt-[-5rem] md:mt-[0rem] lg:mt-[1rem] xl:mt-[2rem] w-auto mb-[4rem] pt-[3rem]">
-          <div className="rounded-[20px] md:rounded-[40px] border-2 bg-background/50 backdrop-blur-lg z-30  p-[20px]">
-
+          <div className="rounded-[20px] md:rounded-[40px] z-30  ">
           <video
             loop  
             muted
@@ -19,7 +18,6 @@ export default function Video() {
             >
             <source src={"/video/video.mp4"} type="video/mp4" />
           </video>
-
             </div>
         </div>
       </div>
