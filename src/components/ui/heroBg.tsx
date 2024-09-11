bb4b2cde4c51b7a0e19c107472c046536d315a86
muhammadammar24 <muhammadammar2492@gmail.com>
2024-09-11 15:23:46 +0500

    hero video added

--- src/components/ui/heroBg.tsx
+++ src/components/ui/heroBg.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import Image from "next/image";
 
 export const HeroBg = () => (
   <>
-    <Image
-      src={"/heroBg/heroBg.gif"}
-      alt={""}
-      width={500}
-      height={500}
-      priority
-      className="w-full  "
+    <video
+      src="/heroBg/video.mp4"
+      autoPlay
+      loop
+      muted
+      playsInline
+      className="w-full h-full object-cover"
     />
   </>
 );
