5844c14f146115a6461f7b1426a1f44b979ddbc0
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-09-18 16:39:20 +0500

    Refactor: Update the Enrollment API schemas

--- src/lib/schemas/enrollment.ts
+++ src/lib/schemas/enrollment.ts
@@ -26,6 +26,7 @@ const StudentCourseStatusSchema = z.enum([
 export type StudentCourseStatus = z.infer<typeof StudentCourseStatusSchema>;
 
 export const EnrollCourseRequestSchema = z.object({
+	student_id: z.string(),
 	student_program_id: z.number(),
 	course_batch_program_id: z.number(),
 	class_time_slot_id: z.number(),
@@ -54,7 +55,46 @@ export const EnrollNewStudentRequestSchema = z.object({
 	lab_time_slot_id: z.number(),
 });
 
-export const EnrollNewStudentResponseSchema = z.string();
+const StudentProgramSchema = z.object({
+	is_dropout: z.boolean(),
+	created_at: z.string().datetime(),
+	dropout_reason: z.string().nullable(),
+	updated_at: z.string().datetime(),
+	dropout_date: z.string().nullable(),
+	registration_date: z.string().datetime(),
+	is_graduated: z.boolean(),
+	student_id: z.string(),
+	program_id: z.number(),
+	graduation_date: z.string().nullable(),
+	id: z.number(),
+	program_status: z.enum(["active", "completed", "dropped"]),
+	created_by: z.string(),
+	batch_id: z.number(),
+	is_active: z.boolean(),
+	updated_by: z.string(),
+});
+
+const StudentCourseSchema = z.object({
+	created_at: z.string().datetime(),
+	course_batch_program_id: z.number(),
+	class_time_slot_id: z.number(),
+	lab_time_slot_id: z.number(),
+	student_course_status: StudentCourseStatusSchema,
+	is_graduated: z.boolean(),
+	created_by: z.string(),
+	id: z.number(),
+	updated_at: z.string().datetime(),
+	student_program_id: z.number(),
+	student_id: z.string(),
+	is_active: z.boolean(),
+	is_paid: z.boolean(),
+	updated_by: z.string(),
+});
+
+export const EnrollNewStudentResponseSchema = z.object({
+	student_program: StudentProgramSchema,
+	student_course: StudentCourseSchema,
+});
 
 export type EnrollNewStudentRequest = z.infer<
 	typeof EnrollNewStudentRequestSchema
