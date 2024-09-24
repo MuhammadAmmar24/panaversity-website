15524fbeaff3eaaaad24e99b6b3a6d90375c30f4
muhammadammar24 <jazba685@gmail.com>
2024-09-24 21:18:15 +0500

    add programs/flagship-program

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,24 +1,7 @@
-import { getProgramCoursesWithOpenRegistration } from "@/src/actions/courses";
-import CoursesClient from '@/src/components/programs/courses';
+import React from 'react'
 
-async function fetchCourses() {
-  const query = {
-    program_id: 1,
-    batch_id: 1,
-    limit: 10,
-  };
-
-  const result = await getProgramCoursesWithOpenRegistration(query);
-
-  if (result.type === "success" && result.data) {
-    return result.data.data;
-  } else {
-    throw new Error(result.message);
-  }
-}
-
-export default async function Courses() {
-  const courses = await fetchCourses();
-
-  return <CoursesClient initialCourses={courses} />;
+export default function page() {
+  return (
+    <div>page</div>
+  )
 }
