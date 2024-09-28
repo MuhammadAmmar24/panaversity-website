664b6ef1addb6a8e6bafafb2cce81b456f7636ec
Resheph Inayat <rasafinayat@gmail.com>
2024-09-29 02:44:04 +0500

    Added Pay to proceed Button in Card

--- src/app/(dashboard)/dashboard/Components/CourseCard.tsx
+++ src/app/(dashboard)/dashboard/Components/CourseCard.tsx
@@ -9,10 +9,17 @@ const CourseCard: React.FC<CourseCardProps> = ({
 }) => {
   return (
     <div className="xl:w-2/4 w-full h-full">
-      <h1 className="text-center font-medium sm:text-start text-xl sm:text-2xl md:text-3xl font-poppins mb-4">Enrolled Courses</h1>
+      <h1 className="text-center font-medium sm:text-start text-xl sm:text-2xl md:text-3xl font-poppins mb-4">
+        Enrolled Courses
+      </h1>
       <div className="bg-white h-44 rounded-lg shadow-xl px-8 py-5">
-        <CiMobile1 className="text-4xl bg-gray-200 rounded-full w-auto md:h-12 md:mb-4 mb-2 p-[8px] " />
-        <h2 className="font-poppins font-medium md:text-lg mb-6 sm:mb-4">{title}</h2>
+        <div className="flex justify-between items-center">
+          <CiMobile1 className="text-4xl bg-gray-200 rounded-full w-auto md:h-12 md:mb-4 mb-2 p-[8px] " />
+          <button  className="md:text-[15px] font-medium md:font-semibold text-[10px] text-red-600 h-6 md:h-8 border border-red-600 rounded-full px-1 py-1 md:px-2 hover:text-white hover:bg-red-600 shadow-lg">Pay to Proceed</button>
+        </div>
+        <h2 className="font-poppins font-medium md:text-lg mb-6 sm:mb-4">
+          {title}
+        </h2>
         <div className="flex items-center gap-6">
           {/* Progress bar */}
           <div className="flex-1 bg-gray-200 rounded-full h-4">
