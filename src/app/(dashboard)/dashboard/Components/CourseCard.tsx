1278b0c46c9131d4240fd31693e00855be3e6163
AmmarAamir786 <ammaraamir609@gmail.com>
2024-09-30 11:51:06 +0500

    course card progressbar responsiveness

--- src/app/(dashboard)/dashboard/Components/CourseCard.tsx
+++ src/app/(dashboard)/dashboard/Components/CourseCard.tsx
@@ -13,31 +13,30 @@ const CourseCard: React.FC<CourseCardProps> = ({
         Enrolled Courses
       </h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <div className="bg-white rounded-lg shadow-xl px-4 mobileM:px-6 lg:px-8 py-5">
+        <div className="bg-white rounded-lg shadow-xl px-4 sm:px-8 py-5">
           <div className="flex justify-between items-center mb-6">
-            <CiMobile1 className="text-4xl bg-gray-200 rounded-full w-auto fold:h-8 mobileM:h-10 sm:h-12 p-2" />
-            <button className="text-[8px] xs:text-[10px] mobileM:text-[10px] md:text-[15px] font-medium md:font-semibold text-red-600 h-6 md:h-8 border border-red-600 rounded-full px-1 py-1 md:px-2 hover:text-white hover:bg-red-600 shadow-lg">
-              Pay to Proceed
-            </button>
+            <CiMobile1 className="text-4xl bg-gray-200 rounded-full w-auto md:h-12  p-[8px] " />
+            <button className="md:text-[15px] font-medium md:font-semibold text-[10px] text-red-600 h-6 md:h-8 border border-red-600 rounded-full px-1 py-1 md:px-2 hover:text-white hover:bg-red-600 shadow-lg">Pay to Proceed</button>
           </div>
-          <h2 className="font-poppins font-medium text-base mobileM:text-lg md:text-xl mb-2">
+          <h2 className="font-poppins font-medium text-lg md:text-xl mb-2">
             {title}
           </h2>
           <div className="flex items-center gap-6">
             {/* Progress bar */}
-            <div className="flex-1 bg-gray-200 rounded-full h-2 mobileM:h-3 md:h-4">
+            <div className="flex-1 bg-gray-200 rounded-full h-2 md:h-4">
               <div
-                className="bg-accent h-2 mobileM:h-3 md:h-4 rounded-full"
+                className="bg-accent h-2 md:h-4 rounded-full"
                 style={{ width: `${progress}%` }}
               ></div>
             </div>
-            <p className="text-gray-500 text-[10px] mobileM:text-xs sm:text-sm md:text-lg">
+            <p className="text-gray-500 text-xs sm:text-sm md:text-lg">
               <span className="text-black">{progress}/</span>
               {lessons} Lessons
             </p>
           </div>
         </div>
       </div>
+
     </div>
   );
 };
