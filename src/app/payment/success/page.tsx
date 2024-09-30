4fb388bda057ac3c0d74b215cd87c0653c8fdc63
DevHusnainAi <syedhussnaintirmizi@gmail.com>
2024-09-30 18:42:44 +0500

    update

---
+++ src/app/payment/success/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div>
+        <h3 className="text-bold text-accent">Success</h3>
+      </div>
+    </div>
+  );
+}
