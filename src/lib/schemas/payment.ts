4eebbe359e7bed1b9ef19999eff6de426202f706
Ahmed <heydude08080@gmail.com>
2024-10-01 19:02:03 +0500

    add schema for payment request body

--- src/lib/schemas/payment.ts
+++ src/lib/schemas/payment.ts
@@ -13,3 +13,16 @@ export const KuickpaySchema = z.object({
 
 // Define the response type
 export type Kuickpay = z.infer<typeof KuickpaySchema>;
+
+
+// Define the schema for the payment request
+export const PaymentRequestSchema = z.object({
+    batch_no: z.number().min(1),
+    package_id: z.number().min(1),
+    student_course_id: z.number().min(1),
+    student_id: z.number().min(1),
+    vendor_type: z.enum(["KUICKPAY"]), // Assuming "KUICKPAY" is the only valid type for now
+  });
+  
+  // Define the type for the payment request
+  export type PaymentRequest = z.infer<typeof PaymentRequestSchema>;
