55de7255c78f742b47583c45a143fa3cd10186ca
Ahmed <heydude08080@gmail.com>
2024-10-02 22:32:49 +0500

    add server action to enroll a student in a course

--- src/actions/enrollment.ts
+++ src/actions/enrollment.ts
@@ -13,6 +13,10 @@ import {
   EnrollStudentRequestSchema,
   EnrollStudentResponse,
   EnrollStudentResponseSchema,
+  EnrollmentRequestSchema,
+  EnrollmentRequest,
+  EnrollmentResponseSchema,
+  EnrollmentResponse,
 } from "@/src/lib/schemas/enrollment";
 import { Result } from "@/src/lib/types";
 import { revalidateTag } from "next/cache";
@@ -198,6 +202,75 @@ export const enrollNewStudentInProgramAndCourse = async (
       data: responseData,
     };
   } catch (error: any) {
+    return {
+      type: "error",
+      message: error.message,
+    };
+  }
+};
+
+// Server action to enroll a student in a course
+export const enrollStudentInACourse = async (
+  enrollmentData: EnrollmentRequest
+): Promise<Result<EnrollmentResponse>> => {
+  try {
+    // Validate enrollment data using the schema
+    const validationResult = EnrollmentRequestSchema.safeParse(enrollmentData);
+
+    if (!validationResult.success) {
+      return {
+        type: "error",
+        message: validationResult.error.errors
+          .map((err) => err.message)
+          .join(", "),
+      };
+    }
+
+    // Construct the API URL
+    const apiUrl = `${process.env.ENROLLMENT_API_URL}/enrollment/courses/enroll`;
+    console.log("API URL:", apiUrl);
+
+    // Make the POST request to your enrollment backend
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.TOKEN_SECRET}`, // Use secret from environment
+      },
+      body: JSON.stringify(validationResult.data), // Convert validated data to JSON string
+    });
+
+    if (!response.ok) {
+      console.log(
+        `Failed to enroll student. Status: ${response.status}, StatusText: ${response.statusText}`
+      );
+      throw new Error(`Failed to enroll student: ${response.statusText}`);
+    }
+
+    // Parse the JSON response
+    const responseData = await response.json();
+    console.log("Enrollment Response:", responseData);
+
+    // Validate the response using the schema
+    const parsedResponse = EnrollmentResponseSchema.safeParse(responseData);
+
+    if (!parsedResponse.success) {
+      console.log("Schema validation failed:", parsedResponse.error.errors);
+      return {
+        type: "error",
+        message: parsedResponse.error.errors
+          .map((err) => err.message)
+          .join(", "),
+      };
+    }
+
+    return {
+      type: "success",
+      message: "Student enrolled successfully",
+      data: parsedResponse.data,
+    };
+  } catch (error: any) {
+    console.error("Error enrolling student:", error.message);
     return {
       type: "error",
       message: error.message,
