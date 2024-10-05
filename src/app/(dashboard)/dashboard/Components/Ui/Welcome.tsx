1e060ae4c4d4a6a5b12de5cc8294801e24f6b957
AmmarAamir786 <ammaraamir609@gmail.com>
2024-10-05 12:49:27 +0500

    welcome now shows only first name

--- src/app/(dashboard)/dashboard/Components/Ui/Welcome.tsx
+++ src/app/(dashboard)/dashboard/Components/Ui/Welcome.tsx
@@ -11,22 +11,22 @@ const Welcome: React.FC = () => {
       try {
         const user_data = await checkUserVerification();
         console.log(user_data)
-  
+
         setUserName(user_data.full_name);
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     }
-  
-      fetchUserData();
+
+    fetchUserData();
   }, []); // Empty dependency array ensures this runs only once when the component mounts
 
   return (
-    <section className=" mx-auto">
+    <section className="mx-auto">
       {/* Welcome message container */}
       <div className="h-32 w-full mt-10 mobileM:mt-12 xs:mt-14 flex gap-4 items-center justify-start">
         <h1 className="font-medium text-start text-xl fold:text-lg mobileM:text-2xl md:text-4xl font-poppins">
-          Welcome {userName} {/* Dynamically display the user's name */}
+          Welcome {userName?.split(' ')[0]} {/* Dynamically display the user's first name */}
           <br />
           <span className="text-xs fold:text-sm mobileM:text-base md:text-lg font-medium">
             Overview of your courses {/* Subtitle */}
