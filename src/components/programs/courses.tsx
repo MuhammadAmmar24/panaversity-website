bf4185b2b88e304a3dc029a3f1d5743019464931
Muhammad Shaheer Kamran <shaheerkamran13@gmail.com>
2024-10-06 01:12:08 +0500

    Skeleton Loading Addition

--- src/components/programs/courses.tsx
+++ src/components/programs/courses.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React, { useState } from "react";
+import { Suspense } from "react";
 import Image from "next/image";
 import Link from "next/link"
 // import rabbit from "@/";
 import { Course } from "@/src/lib/schemas/courses";
-
+import { CoursesSkeleton } from "@/src/app/(dashboard)/dashboard/Components/Skeleton/CoursesSkeleton";
 const CourseCard = ({ course }: { course: Course }) => (
+  
   <Link
   href={`/programs/flagship-program/${course.course_id}`}
   >
@@ -54,14 +56,17 @@ const CoursesClient = ({ initialCourses }: { initialCourses: Course[] }) => {
         </div>
 
         {/* courses */}
+
+      
         <div className="lg:max-w-[990px] xl:max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8 mt-8">
           <h2 className=" mt-10 mb-10 font-poppins text-md sm:text-lg gradient-border border-black font-medium border-b rounded-[100px]   w-fit  uppercase tracking-wide">APPLIED GEN AI COURSES</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-12 md:gap-20 justify-items-center md:justify-items-stretch">
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-12 md:gap-20 justify-items-center md:justify-items-stretch">   
             {initialCourses.slice(0, visibleCourses).map((course, index) => (
               <CourseCard key={index} course={course} />
             ))}
           </div>
         </div>
+      
 
         {/* Load More Button */}  
         {visibleCourses < initialCourses.length && (
