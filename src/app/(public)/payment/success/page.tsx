c25899f08bb78e585a2aa43481c9d979c700163e
DevHusnainAi <syedhussnaintirmizi@gmail.com>
2024-10-07 11:30:03 +0500

    update

--- src/app/(public)/payment/success/page.tsx
+++ src/app/(public)/payment/success/page.tsx
@@ -1,17 +1,16 @@
-import PaymentSuccess from '@/src/components/payment/payment-success'
-import React, { Suspense } from 'react'
+import PaymentSuccess from "@/src/components/payment/payment-success";
+import React, { Suspense } from "react";
 
 const page = () => {
   return (
-    <div className='flex justify-center mt-[-4rem] items-center px-[1rem] min-h-screen'>
-            <div className="bg-white p-4  sm:p-8 rounded-xl shadow-md ">
-
+    <div className="flex justify-center mt-[-4rem] items-center px-[1rem] min-h-screen">
+      <div className="bg-white p-4  sm:p-8 rounded-xl shadow-md ">
         <Suspense>
-            <PaymentSuccess />
+          <PaymentSuccess />
         </Suspense>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
