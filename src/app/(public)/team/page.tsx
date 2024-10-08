3c79c65bcecdfc42477663f5a2629b762dfe8d5b
AmmarAamir786 <ammaraamir609@gmail.com>
2024-10-08 19:08:38 +0500

    remove metadata for team/page.tsx

--- src/app/(public)/team/page.tsx
+++ src/app/(public)/team/page.tsx
@@ -4,13 +4,7 @@ import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import { allTeamMembers } from "@/src/constants/teams";
-import CardSkeleton from "@/src/components/ui/skeletons/CardSkeleton";import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Team Panaversity",
-  description: `Meet the Panaversity team of experts specializing in Generative AI, cloud-native technologies, and AI Agents creation. Learn about our great minds behind our innovative learning platform.`
-};
-
+import CardSkeleton from "@/src/components/ui/skeletons/CardSkeleton";
 
 type SocialLink = {
   href: string;
