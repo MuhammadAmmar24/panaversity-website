f22eeeafda44159b56ab7a7ab9431302c4c7e15a
AmmarAamir786 <ammaraamir609@gmail.com>
2024-10-08 18:43:42 +0500

    add metadata for programs/layout.tsx

--- src/app/(public)/programs/layout.tsx
+++ src/app/(public)/programs/layout.tsx
@@ -2,8 +2,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Courses Offered",
-  description: ``
+  title: "Programs offered by Panaversity",
+  description: `Discover Panaversity's comprehensive programs in Generative AI, cloud-native technologies, and custom GPT development. Our expertly designed courses empower global learners to master the latest innovations in AI and technology, preparing them for future opportunities in a rapidly evolving digital world.`
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
