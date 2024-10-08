7ec95ec8ff4134d46142d420430d951219056f96
AmmarAamir786 <ammaraamir609@gmail.com>
2024-10-08 10:33:07 +0500

    program overview section ui change

--- src/constants/programOverview.ts
+++ src/constants/programOverview.ts
@@ -16,20 +16,20 @@ export const features = [
     iconSize: { width: 24, height: 24 },
   },
   {
-    title: "Real-World Projects",
-    description:
-      "Work on projects that mirror real-world tech challenges to build practical skills.",
-    icon: "/images/bar-chart-3-64.png",
-    bgColor: "#4B62E6",
-    iconSize: { width: 24, height: 24 },
-  },
-  {
     title: "Cutting-Edge Technology",
     description:
       "Stay ahead with hands-on experience using the latest AI tools and platforms.",
     icon: "/images/icons8-light-bulb-96.png",
     bgColor: "#F786BE",
     iconSize: { width: 26, height: 26 },
+  },
+  {
+    title: "Real-World Projects",
+    description:
+      "Work on projects that mirror real-world tech challenges to build practical skills.",
+    icon: "/images/bar-chart-3-64.png",
+    bgColor: "#4B62E6",
+    iconSize: { width: 24, height: 24 },
   },
   {
     title: "Flexible Learning",
