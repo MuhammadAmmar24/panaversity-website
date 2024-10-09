7bd2211f6bd44bb68ba255d8288261b2d476d5ff
DevHusnainAi <syedhussnaintirmizi@gmail.com>
2024-10-09 11:10:31 +0500

    update

--- src/components/ui/GetEnrolled.tsx
+++ src/components/ui/GetEnrolled.tsx
@@ -4,22 +4,14 @@ import { getTimeSlotsForCourseBatchProgram } from "@/src/actions/courses";
 import { enrollNewStudentInProgramAndCourse } from "@/src/actions/enrollment"; // Import the action
 import { useRouter } from "next/navigation";
 import { getCoursePrice } from "@/src/actions/courses";
+import { checkUserVerification } from "@/src/actions/profile";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-
-interface CourseSheetProps {
-  program_id: string;
-  batch_id: number;
-  course_batch_program_id: number;
-  profile_id: string;
-}
-
 
 export default function GetEnrolled({
   program_id,
   batch_id,
   course_batch_program_id,
-  profile_id,
-}: CourseSheetProps) {
+}: any) {
   const [classTimeSlots, setClassTimeSlots] = useState<any[]>([]);
   const [selectedDay, setSelectedDay] = useState("");
   const [selectedTimeSlot, setSelectedTimeSlot] = useState(""); // Storing label of time slot
@@ -35,7 +27,8 @@ export default function GetEnrolled({
   const [isEnrolled, setIsEnrolled] = useState(false);
   const [enrollmentError, setEnrollmentError] = useState<string | null>(null);
   const [isPending, startTransition] = useTransition();
-  const paymentMethods = ["Stripe", "Kuickpay"];
+  const [profile, setProfile] = useState<ProfileData | null>(null);
+  const paymentMethods = ["Kuickpay", "Stripe"];
   const [focusedInput, setFocusedInput] = useState("");
 
   // Fetch time slots
@@ -82,7 +75,18 @@ export default function GetEnrolled({
       }
     };
 
+    const fetchUserData = async () => {
+      try {
+        const user_data = await checkUserVerification();
+
+        setProfile(user_data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
     fetchTimeSlots();
+    fetchUserData();
     fetchEnrollmentPrice();
   }, []);
 
@@ -153,7 +157,7 @@ export default function GetEnrolled({
     if (!isDayAndTimeSelected) return;
 
     const payload: any = {
-      student_id: profile_id,
+      student_id: profile?.id,
       program_id: program_id,
       batch_id: batch_id,
       course_batch_program_id: course_batch_program_id,
@@ -163,17 +167,16 @@ export default function GetEnrolled({
     };
 
     // Log the payload for debugging
+    console.log("Enrollment Payload:", payload);
 
     startTransition(async () => {
       try {
         const result: any = await enrollNewStudentInProgramAndCourse(payload);
-        console.log(result)
-
+        console.log("RESULT", result);
         const url = result.data?.fee_voucher?.stripe?.stripe_url;
-
+        console.log("URL", url);
 
         if (result.type === "success") {
-          // revalidatePath("/dashboard");
           setIsEnrolled(true); // Enrollment success, show message
           if (url) {
             console.log("URL", url);
@@ -374,7 +377,7 @@ export default function GetEnrolled({
             {isPending ? (
               <>
                 <AiOutlineLoading3Quarters className="mr-2 h-4 w-4 animate-spin" />
-                Processing...
+                Enrolling...
               </>
             ) : (
               "Enroll"
