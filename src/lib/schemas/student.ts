81c0a49b9a464a9bc838b15f13d31c88b213b023
Ahmed <heydude08080@gmail.com>
2024-10-09 20:11:15 +0500

    add schema for student  profile

--- src/lib/schemas/student.ts
+++ src/lib/schemas/student.ts
@@ -12,3 +12,23 @@ export const get_profile = z.object({
 
 // Type inferred from the response schema
 export type GetProfileResponse = z.infer<typeof get_profile>;
+
+
+// Schema for the request body to update student profile
+export const updateProfileSchema = z.object({
+  address: z.string(),
+  city: z.string(),
+  country: z.string(),
+  is_active: z.boolean(),
+});
+
+// Type inferred from the request schema
+export type RequestBody = z.infer<typeof updateProfileSchema>;
+
+// Schema for the response body after updating student profile
+export const updateProfileResponseSchema = z.object({
+  message: z.string(),
+});
+
+// Type inferred from the response schema
+export type ResponseBody = z.infer<typeof updateProfileResponseSchema>;
