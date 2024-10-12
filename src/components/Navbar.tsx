521ab892ba92a69d05187e29da266921263d18ad
AmmarAamir786 <ammaraamir609@gmail.com>
2024-10-12 16:34:41 +0500

    add dashboard button in mobnavbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -75,9 +75,8 @@ export default function Navbar() {
 
   return (
     <header
-      className={`py-1 sm:py-4 sticky bg-white/50 backdrop-blur-lg top-0 z-40 w-full transition-transform duration-500 ${
-        hidden ? "-translate-y-full" : "translate-y-0"
-      }`}
+      className={`py-1 sm:py-4 sticky bg-white/50 backdrop-blur-lg top-0 z-40 w-full transition-transform duration-500 ${hidden ? "-translate-y-full" : "translate-y-0"
+        }`}
     >
       <div className="container mx-auto flex h-16 lg:max-w-[950px] xl:max-w-6xl items-center justify-between mobileM:px-3 xs:px-1 sm:px-2  md:px-0">
         {/* Logo */}
@@ -86,7 +85,7 @@ export default function Navbar() {
             src={logo}
             alt="Panaversity Logo"
             className="w-[120px] sm:w-[140px] md:w-[140px] lg:w-[150px]"
-           
+
           />
         </Link>
 
@@ -96,11 +95,10 @@ export default function Navbar() {
             <Link
               key={nav.name}
               href={nav.link}
-              className={` text-base ${
-                pathName === nav.link
+              className={` text-base ${pathName === nav.link
                   ? "text-[#40e477]"
                   : "text-textPrimary hover:text-[#40e477]"
-              }`}
+                }`}
             >
               {nav.name}
             </Link>
@@ -165,6 +163,18 @@ export default function Navbar() {
                     </Link>
                   </SheetClose>
                 ))}
+                <Link
+                  href={IsLoggedIn ? "/dashboard" : "/register"}
+                  //  onClick={handleClick}
+                  className="relative items-center justify-start text-center inline-block px-3 py-2  overflow-hidden font-medium rounded-3xl group bg-accent"
+                >
+                  <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-accent opacity-[3%]"></span>
+                  <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-accent opacity-100 group-hover:-translate-x-8"></span>
+                  <span className="relative w-full text-center text-md text-textPrimary transition-colors duration-200 ease-in-out group-hover:text-white font-poppins font-medium">
+                    {IsLoggedIn ? "Dashboard" : "Get Started"}
+                  </span>
+                  <span className="absolute inset-0 border-2 border-accent rounded-3xl"></span>
+                </Link>
               </nav>
             </SheetContent>
           </Sheet>
