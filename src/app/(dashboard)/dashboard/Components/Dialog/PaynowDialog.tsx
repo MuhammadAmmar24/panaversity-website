91ef4ea70f5c3b08fd903ba304f29ecb31635e63
AmmarAamir786 <ammaraamir609@gmail.com>
2024-10-13 11:47:13 +0500

    dashboard payment is now working

--- src/app/(dashboard)/dashboard/Components/Dialog/PaynowDialog.tsx
+++ src/app/(dashboard)/dashboard/Components/Dialog/PaynowDialog.tsx
@@ -34,7 +34,7 @@ export default function PaymentDialog({
   };
 
 
-  const paymentMethods = ["Stripe"];
+  const paymentMethods = ["STRIPE"];
 
   // Automatically select Stripe if it's the only payment method
   useEffect(() => {
@@ -56,45 +56,6 @@ export default function PaymentDialog({
               Please select your preferred payment method and confirm the
               payment.
             </Dialog.Description>
-
-
-            {/* <div className="mb-4">
-              <label
-                className="block text-sm font-medium mb-2"
-                htmlFor="payment-method"
-              >
-                Payment Method
-              </label>
-              <div className="relative w-full">
-                <select
-                  id="payment-method"
-                  className="w-full p-3 pr-10 appearance-none text-gray-700 focus:outline-none bg-transparent border border-accent focus:border-accent focus:ring-accent rounded-lg"
-                  value={selectedMethod}
-                  onChange={(e) => setSelectedMethod(e.target.value)}
-                  disabled={isLoading} // Disable the select field when loading
-                >
-                  <option value="" disabled hidden>
-                    Select Payment Method
-                  </option>{" "}
-                  <option value="STRIPE">STRIPE</option>
-                  <option value="KUICKPAY">KUICKPAY</option>
-                </select>
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                  <svg
-                    className="w-5 h-5 text-gray-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div> */}
 
             <div className="mb-4">
               <label
