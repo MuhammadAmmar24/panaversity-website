d9a43b14e4d772afea2b36f3dda3d1d1af353205
Em-Aar <mmrhaq@gmail.com>
2024-10-16 12:25:07 +0500

    Remove payment in profile

--- src/app/(dashboard)/dashboard/Components/Ui/TopbarDropdown.tsx
+++ src/app/(dashboard)/dashboard/Components/Ui/TopbarDropdown.tsx
@@ -137,15 +137,6 @@ const Dropdown: React.FC<DropdownProps> = ({
             </li>
           </Link>
 
-          {/* Payments section */}
-          {/* <li
-            className="flex items-center space-x-2 p-2 hover:bg-gray-100 rounded-lg cursor-pointer"
-            role="menuitem"
-          >
-            <TfiWallet className="w-5 h-5 text-gray-700" />
-            <span className="text-gray-700 text-xs sm:text-sm">Payments</span>
-          </li> */}
-
           {/* Help center */}
           <li
             className="flex items-center space-x-2 p-2 hover:bg-gray-100 rounded-lg cursor-pointer"
