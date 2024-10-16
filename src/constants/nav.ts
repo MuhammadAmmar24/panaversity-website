de807943e4f8c48f166991e11f561be8452ca4ad
Em-Aar <mmrhaq@gmail.com>
2024-10-16 12:34:10 +0500

    Add contact icon in navbar sheet

--- src/constants/nav.ts
+++ src/constants/nav.ts
@@ -1,6 +1,6 @@
-import { FaHome, FaUsers, FaBookOpen, FaBullhorn } from "react-icons/fa";
-import { GrContact } from "react-icons/gr";
+import { FaBookOpen, FaHome, FaUsers } from "react-icons/fa";
 import { IconType } from "react-icons/lib";
+import { MdEmail } from "react-icons/md";
 
 
 type NavItem = {
@@ -13,6 +13,6 @@ export const navItems: NavItem[] = [
   { name: "Home", link: "/", icon: FaHome},
   { name: "Our Team", link: "/team", icon: FaUsers },
   { name: "Programs", link: "/programs/flagship-program", icon: FaBookOpen },
-  { name: "Contact Us", link: "/contact", icon: GrContact },
+  { name: "Contact Us", link: "/contact", icon: MdEmail },
 ];
 
