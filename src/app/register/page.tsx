1a4755443f48c6d1467cbcc86e95737e66278c41
Em-Aar <mmrhaq@gmail.com>
2024-10-23 07:01:47 +0500

    Remove use-client in CardWrapper of register route

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -44,9 +44,7 @@ const RegisterPage = () => {
       <main>
         <div className="flex flex-col justify-center items-center min-h-screen">
           <div className="mt-10 mb-20">
-            <Suspense>
               <RegisterFormPage />
-            </Suspense>
           </div>
         </div>
       </main>
