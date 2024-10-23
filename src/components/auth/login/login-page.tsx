ce635612bdea3df8a0dfd55b407dafa85e0b4344
Em-Aar <mmrhaq@gmail.com>
2024-10-23 11:57:43 +0500

    Update middleware for cache control

--- src/components/auth/login/login-page.tsx
+++ src/components/auth/login/login-page.tsx
@@ -1,6 +1,8 @@
 import { CardWrapper } from "@/src/components/auth/card-wrapper";
 import { LoginForm } from "./login-form";
 
+export const dynamic = 'force-static'
+
 export const LoginFormPage = () => {
   return (
     <CardWrapper headerLabel="Login">
