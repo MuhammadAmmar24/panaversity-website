007251581d543068f1ba8a89356c1cc3b97a029f
Em-Aar <mmrhaq@gmail.com>
2024-10-24 22:27:43 +0500

    Add Responsiveness in register dialog

--- src/app/@modal/(.)register/page.tsx
+++ src/app/@modal/(.)register/page.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from "react";
 import RegisterDialog from "../../../components/auth/register/register-dialog";
 
 export default function RegisterModal() {
   return (
-    <Suspense>
       <RegisterDialog />
-    </Suspense>
   );
 }
--- src/components/auth/register/register-dialog.tsx
+++ src/components/auth/register/register-dialog.tsx
@@ -12,8 +12,8 @@ export default function RegisterDialog() {
   return (
     <Dialog.Root open={true} onOpenChange={() => router.back()}>
       <Dialog.Portal>
-        <Dialog.Overlay className="z-40 fixed inset-0 bg-black/70 backdrop-blur-sm" />
-        <Dialog.Content className="z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-background  rounded-lg p-5 w-[300px] mobileM:w-[350px] xs:w-[400px] md:w-[400px] overflow-y-auto ">
+        <Dialog.Overlay className="z-40 fixed inset-0 bg-black/70 backdrop-blur-sm " />
+        <Dialog.Content className="z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-background  rounded-lg p-6 min-w-[18rem]  max-w-sm max-h-[94dvh] sm:w-full overflow-y-auto xl:overflow-y-hidden">
           <Dialog.Title className="text-lg font-bold mb-4">
             Create an account
           </Dialog.Title>
