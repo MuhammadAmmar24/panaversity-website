4205286ff985a4bd0a2dd0dc4baa9eaa8353019e
muhammadammar24 <jazba685@gmail.com>
2024-10-25 18:24:49 +0500

    remove register  overflow

--- src/components/auth/register/register-dialog.tsx
+++ src/components/auth/register/register-dialog.tsx
@@ -13,7 +13,7 @@ export default function RegisterDialog() {
     <Dialog.Root open={true} onOpenChange={() => router.back()}>
       <Dialog.Portal>
         <Dialog.Overlay className="z-40 fixed inset-0 bg-black/70 backdrop-blur-sm " />
-        <Dialog.Content className="z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-background  rounded-lg p-6 min-w-[18rem]  max-w-sm max-h-[94dvh] sm:w-full overflow-y-auto xl:overflow-y-hidden">
+        <Dialog.Content className="z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-background  rounded-lg p-6 min-w-[18rem]  max-w-sm max-h-[94dvh] sm:w-full overflow-y-auto ">
           <Dialog.Title className="text-lg font-bold mb-4">
             Create an account
           </Dialog.Title>
