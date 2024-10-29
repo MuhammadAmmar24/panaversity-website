d9b59c36b4c3ab50f2461dd769a4c644ad2f649a
AmmarAamir786 <ammaraamir609@gmail.com>
2024-10-29 17:47:36 +0500

    remove console from tokenvalidy.ts

--- src/lib/tokenValidity.ts
+++ src/lib/tokenValidity.ts
@@ -5,9 +5,7 @@ export async function isValidToken(){
     const get_cookies = cookies().get("user_data")?.value;
     if (get_cookies) {
       const access_token= JSON.parse(get_cookies).access_token
-      console.log(access_token, "Access Token")
       const isTokenExpired = await check_token_expiry(access_token);
-      console.log(isTokenExpired, "isTokenExpired");
       if(isTokenExpired || isTokenExpired === null){ 
       return false
     } else {
