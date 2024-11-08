d0a369c6991a67309d7f33cf6d7ee6095eeefc27
Em-Aar <mmrhaq@gmail.com>
2024-11-08 21:44:36 +0500

    Update verify_token function with jose

--- src/lib/verify_token.ts
+++ src/lib/verify_token.ts
@@ -1,18 +1,45 @@
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
+
+// export async function check_token_expiry(access_token: string) {
+//   try {
+//     const decodedToken = jwt.decode(access_token) as { exp: number };
+
+//     // Check if the token has expired
+//     if (decodedToken.exp * 1000 < Date.now()) {
+//       console.log("verify_token");
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   } catch (error) {
+//     return null;
+//   }
+// }
+
+
+import { decodeJwt } from "jose";
 
 export async function check_token_expiry(access_token: string) {
   try {
-    const decodedToken = jwt.decode(access_token) as { exp: number };
+    // Decode the JWT payload without verifying
+    const payload = decodeJwt(access_token);
 
-    // Check if the token has expired
-    if (decodedToken.exp * 1000 < Date.now()) {
+    // Check if 'exp' exists and return it
+    if (payload.exp !== undefined) {
+      console.log(payload.exp * 1000 < Date.now(), "Token expiry")
+      if(payload.exp * 1000 < Date.now()){
+        return true
+      }
+      else {
+        return false
+      }
 
-    
-      return true;
     } else {
-      return false;
+      return null;
     }
   } catch (error) {
+    console.error("Error decoding token:", error);
     return null;
   }
 }
+
