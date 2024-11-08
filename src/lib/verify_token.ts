25c3bee9e66c85d3a7740afa5637f693a75afa87
Em-Aar <mmrhaq@gmail.com>
2024-11-09 00:15:26 +0500

    update code for zero remaining seats

--- src/lib/verify_token.ts
+++ src/lib/verify_token.ts
@@ -1,22 +1,3 @@
-// import jwt from "jsonwebtoken";
-
-// export async function check_token_expiry(access_token: string) {
-//   try {
-//     const decodedToken = jwt.decode(access_token) as { exp: number };
-
-//     // Check if the token has expired
-//     if (decodedToken.exp * 1000 < Date.now()) {
-//       console.log("verify_token");
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   } catch (error) {
-//     return null;
-//   }
-// }
-
-
 import { decodeJwt } from "jose";
 
 export async function check_token_expiry(access_token: string) {
