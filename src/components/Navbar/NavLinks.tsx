e35d70775bb884563284476359c1aa47719abe02
Em-Aar <mmrhaq@gmail.com>
2024-11-14 22:27:05 +0500

    update navbar hover

--- src/components/Navbar/NavLinks.tsx
+++ src/components/Navbar/NavLinks.tsx
@@ -1,31 +1,25 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { navItems } from "@/src/constants/nav";
 import { usePathname } from "next/navigation";
 
-
 const NavLinks = () => {
-
-      const pathName = usePathname();
+  const pathName = usePathname();
 
   return (
     <nav className="hidden items-center mt-6 gap-10 text-sm font-medium md:flex">
-    {navItems.map((nav) => (
-      <Link
-              key={nav.name}
-              href={nav.link}
-              aria-label= {`${nav.name}`}
-              className={` text-base ${
-                pathName === nav.link
-                  ? "text-[#40e477]"
-                  : "text-textPrimary hover:text-[#40e477]"
-              }`}
-            >
-              {nav.name}
-            </Link>
-    ))}
-  </nav>
-  )
-}
+      {navItems.map((nav) => (
+        <Link
+          key={nav.name}
+          href={nav.link}
+          aria-label={`${nav.name}`}
+          className={`text-base ${pathName === nav.link ? "nav nav-active" : "nav"}`}
+        >
+          {nav.name}
+        </Link>
+      ))}
+    </nav>
+  );
+};
 
 export default NavLinks;
