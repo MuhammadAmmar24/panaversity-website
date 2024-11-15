27e84f8685e649647d02fabd2d63ceaec847feab
Em-Aar <mmrhaq@gmail.com>
2024-11-15 15:59:45 +0500

    Update sytling of consent cookie

--- src/components/Cookies/cookieConsent.tsx
+++ src/components/Cookies/cookieConsent.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Cookies from "js-cookie";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function CookieConsent() {
@@ -25,24 +26,25 @@ export default function CookieConsent() {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-gray-500/30 backdrop-blur-md text-gray-900 p-4 flex flex-col md:flex-row justify-between items-center z-50">
-      <p className="mb-4 md:mb-0 text-center md:text-left">
-        We use cookies to ensure user get the best experience on our website.
+    <aside className="p-4 w-full sm:max-w-[350px] fixed bottom-2 right-0 sm:right-2 flex flex-col gap-y-6 justify-between items-center  rounded-lg bg-white/40 backdrop-blur-md border border-gray-400/50 z-50">
+      <p className="mb-4 md:mb-0 text-gray-900 text-center md:text-left">
+        We use cookies to ensure user get the best experience on our website. Checkout our{" "}
+        <Link href={'/privacy-policy/#consent-policy'} aria-label="Read more aboutContent Policy" className=" text-gray-900 underline underline-offset-4 decoration-2 decoration-accent">Consent Policy.</Link>
       </p>
-      <div className="space-x-3">
+      <div className="space-x-8">
         <button
           onClick={handleAccept}
-          className="bg-accent hover:bg-white text-white hover:text-accent py-2 px-4 rounded-lg mr-2"
+          className="bg-accent hover:bg-white text-white hover:text-accent py-2 px-4 rounded-lg"
         >
           Accept
         </button>
         <button
           onClick={handleReject}
-          className="bg-gray-600 hover:bg-white text-white hover:text-gray-600 py-2 px-4 rounded-lg"
+          className="bg-gray-800 hover:bg-white text-white hover:text-gray-600 py-2 px-4 rounded-lg"
         >
           Reject
         </button>
       </div>
-    </div>
+    </aside>
   );
 }
