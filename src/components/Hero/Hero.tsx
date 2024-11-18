29702af12f7c21f24d0c87f0a48202f790fdf7b5
Em-Aar <mmrhaq@gmail.com>
2024-11-18 23:18:53 +0500

    change hero text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -15,9 +15,8 @@ export default function Hero() {
               by Experts
             </h1>
             <p className="font-inter mt-6 text-[0.9rem] leading-8 text-textSecondary md:text-[1.1rem]">
-              Master Agentic AI augmented with our Personalized, AI-Powered
-              Platform — Tailored for Your{" "}
-              <span className="gradient-border2">Success!</span>
+            Access Live Classes and Labs on Zoom from Anywhere Worldwide - Choose Between Dedicated English or Urdu/Hindi{" "}
+              <span className="gradient-border2">Sections!</span>
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
