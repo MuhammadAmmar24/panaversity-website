ee63a912ea7b1cf2df77835c3c422b1fa43acd59
AmmarAamir786 <ammaraamir609@gmail.com>
2024-11-19 13:28:00 +0500

    Remove extra images

--- src/constants/programOverview.ts
+++ src/constants/programOverview.ts
@@ -12,7 +12,7 @@ export const features = [
     title: "Core Curriculum",
     description:
       "Learn Generative AI, Cloud Native Computing, and Physical AI with our experts.",
-    icon: "/images/icons8-clock-100.png",
+    icon: "/program_overview/clock.webp",
     bgColor: "#FA7F27",
     iconSize: { width: 24, height: 24 },
   },
@@ -20,7 +20,7 @@ export const features = [
     title: "Next-Gen Tech",
     description:
       "Stay ahead with hands-on experience using the latest AI tools and platforms.",
-    icon: "/images/icons8-light-bulb-96.png",
+    icon: "/program_overview/bulb.webp",
     bgColor: "#F786BE",
     iconSize: { width: 26, height: 26 },
   },
@@ -28,7 +28,7 @@ export const features = [
     title: "Real-World Projects",
     description:
       "Work on projects that mirror real-world tech challenges to build practical skills.",
-    icon: "/images/bar-chart-3-64.png",
+    icon: "/program_overview/chart.webp",
     bgColor: "#4B62E6",
     iconSize: { width: 24, height: 24 },
   },
@@ -36,7 +36,7 @@ export const features = [
     title: "Flexible Learning",
     description:
       "Learn at your own pace with 24/7 access to AI Mentors and community support.",
-    icon: "/images/icons8-brain-100.png",
+    icon: "/program_overview/brain.webp",
     bgColor: "#FFC04C",
     iconSize: { width: 24, height: 24 },
   },
