57bd59796a68a64d2015a5f0768db26eb604a1bd
AmmarAamir786 <ammaraamir609@gmail.com>
2024-11-25 16:17:18 +0500

    fix dropdown visiblity fix

--- src/app/(dashboard)/dashboard/Components/Ui/TopbarDropdown.tsx
+++ src/app/(dashboard)/dashboard/Components/Ui/TopbarDropdown.tsx
@@ -50,7 +50,7 @@ const Dropdown: React.FC<DropdownProps> = ({
   }
 
   return (
-    <div className="relative" ref={dropdownRef}>
+    <div className="relative z-50" ref={dropdownRef}>
       <button
         onClick={toggleDropdown}
         className="flex items-center space-x-2 focus:outline-none"
