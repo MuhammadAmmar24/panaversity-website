9c1c889b4b7ca9ecb2f90680d5fb4ba429c6b483
AmmarAamir786 <ammaraamir609@gmail.com>
2024-11-27 20:22:16 +0500

    Change icon

--- src/app/(dashboard)/dashboard/Components/Ui/PasswordSettings.tsx
+++ src/app/(dashboard)/dashboard/Components/Ui/PasswordSettings.tsx
@@ -24,7 +24,7 @@ import {
   AiOutlineEyeInvisible,
   AiOutlineLoading3Quarters,
 } from "react-icons/ai";
-import { FiAlertTriangle } from "react-icons/fi";
+import { IoInformationCircleOutline } from "react-icons/io5";
 import "react-phone-input-2/lib/style.css";
 import * as z from "zod";
 
@@ -244,8 +244,8 @@ function PasswordSettings({ profile_email }: { profile_email: string }) {
               </div>
             </form>
           </FormProvider>
-          <p className="mt-4 text-xs text-gray-500 flex gap-2 items-center">
-            <FiAlertTriangle className="text-red-600"/> After changing your password, you’ll be logged out and need to log in again.
+          <p className="mt-4 text-xs text-gray-500 flex gap-1 items-center">
+            <IoInformationCircleOutline className="text-black text-sm"/> After changing your password, you’ll be logged out and need to log in again.
           </p>
         </div>
       )}
